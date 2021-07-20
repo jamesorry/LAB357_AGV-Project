c0c0b9ef9b4fdfabe355a522fef95962eeb45cdf
jamesorry <jamesnail333@gmail.com>
2021-07-20 22:50:06 +0800

    20210720

--- src/PIO_ControlBoard/src/EEPROM_Function.cpp
+++ src/PIO_ControlBoard/src/EEPROM_Function.cpp
@@ -2,85 +2,73 @@
 #include "MainProcess.h"
 #include "hmi.h"
 
-#define EEPROM_DEBUG	0
+#define EEPROM_DEBUG 0
 
 extern MainDataStruct maindata;
 extern HardwareSerial *cmd_port;
 
-
 void READ_EEPROM()
-{ 
-// 讀取EEPROM，回傳值count代表總共寫入幾個byte 
- 
+{
+  // 讀取EEPROM，回傳值count代表總共寫入幾個byte
+
   int count = EEPROM_readAnything(0, maindata);
-  #if EEPROM_DEBUG
-  	if(cmd_port != NULL)
-	{
-  		cmd_port->println("Read EEPROM.");
-  		cmd_port->print("Vendor: ");
-  		cmd_port->println(maindata.Vendor);
-  	}
-  #endif
-  
-  if (String(maindata.Vendor) != VENDOR)  //未定義 時寫入初值 NNNNNNNNNNNN  
+#if EEPROM_DEBUG
+  if (cmd_port != NULL)
   {
-     Clear_EEPROM();
+    cmd_port->println("Read EEPROM.");
+    cmd_port->print("Vendor: ");
+    cmd_port->println(maindata.Vendor);
   }
-  
-//  Serial.println(KEEP_DATA.Vendor);
-//  Serial.println(KEEP_DATA.RS485_ID);
+#endif
 
-//  Serial.print(count);
-//  Serial.println(" bytes read.");
-  
-
+  if (String(maindata.Vendor) != VENDOR) //未定義 時寫入初值 NNNNNNNNNNNN
+  {
+    Clear_EEPROM();
+  }
 }
 
 void WRITE_EEPROM()
-{  
-  // 寫入EEPROM，回傳值count代表總共寫入幾個byte 
+{
+  // 寫入EEPROM，回傳值count代表總共寫入幾個byte
   int count = EEPROM_writeAnything(0, maindata);
-//  Serial.print(count);
-//  Serial.println(" bytes written.");
-//  Serial.println("---------------");
-  
-//	if(cmd_port != NULL) 
-//		cmd_port->println("WRITE_EEPROM End.");
+  //  Serial.print(count);
+  //  Serial.println(" bytes written.");
+  //  Serial.println("---------------");
+
+  //	if(cmd_port != NULL)
+  //		cmd_port->println("WRITE_EEPROM End.");
 }
 
-
-void   Clear_EEPROM()
-{ 
-	uint8_t i;
-#if EEPROM_DEBUG	
-  	if(cmd_port != NULL)
-	{
-  		cmd_port->println("Clear EEPROM.");
-  	}
+void Clear_EEPROM()
+{
+  uint8_t i;
+#if EEPROM_DEBUG
+  if (cmd_port != NULL)
+  {
+    cmd_port->println("Clear EEPROM.");
+  }
 #endif
-	strcpy(maindata.Vendor, VENDOR); 
-	
-// 寫入EEPROM，回傳值count代表總共寫入幾個byte 
-   int   count = EEPROM_writeAnything(0, maindata);
-     
+  strcpy(maindata.Vendor, VENDOR);
+
+  // 寫入EEPROM，回傳值count代表總共寫入幾個byte
+  int count = EEPROM_writeAnything(0, maindata);
 }
 
-template <class T> int EEPROM_writeAnything(int address, const T &data)
+template <class T>
+int EEPROM_writeAnything(int address, const T &data)
 {
   const byte *p = (const byte *)(const void *)&data;
   int i, n;
-  for(i = 0, n = sizeof(data); i < n; i++)
+  for (i = 0, n = sizeof(data); i < n; i++)
     EEPROM.write(address++, *p++);
   return i;
 }
-template <class T> int EEPROM_readAnything(int address, T &data)
+template <class T>
+int EEPROM_readAnything(int address, T &data)
 {
   byte *p = (byte *)(void *)&data;
   int i, n;
-  for(i = 0, n = sizeof(data); i < n; i++)
+  for (i = 0, n = sizeof(data); i < n; i++)
     *p++ = EEPROM.read(address++);
   return i;
 }
-
-
-
