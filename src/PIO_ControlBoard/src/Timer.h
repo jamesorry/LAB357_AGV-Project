db8b8d73411e6ecb98e716049ecd4dd5a44004f3
jamesorry <jamesnail333@gmail.com>
2022-01-14 02:15:51 +0800

    update

--- src/PIO_ControlBoard/src/Timer.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TIMER_H_
-#define _TIMER_H_
-
-#define TIMER_INTERVAL_MS	10
-void TimerInit(unsigned char timer, unsigned long microSec, unsigned short ocrna, unsigned int tccrnb);
-void TimerInit(unsigned char timer, unsigned long microSec);
-
-#endif
